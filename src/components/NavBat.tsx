cfd6c33d96c70bacb08ddc2f87ee7b2f82a3720a
avacodehub <avacodehub@gmail.com>
2020-09-11 01:37:03 +0300

    Init with login/logout/register

---
+++ src/components/NavBat.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Box, Link, Flex, Button } from "@chakra-ui/core";
+import NextLink from "next/link";
+import { useMeQuery, useLogoutMutation } from "../generated/graphql";
+import { isServer } from "../utils/isServer";
+interface NavBarProps {}
+
+export const NavBar: React.FC<NavBarProps> = ({}) => {
+  const [{fetching: logoutFetching}, logout] = useLogoutMutation();
+  const [{ data, fetching }] = useMeQuery({
+    pause: isServer()
+  });
+  let body = null;
+
+  if (fetching) {
+    //data is loading
+    body = null;
+  } else if (!data?.me) {
+    //user is
+    body = (
+      <>
+        <NextLink href="/login">
+          <Link mr={4}>Login</Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link>Register</Link>
+        </NextLink>
+      </>
+    );
+  } else {
+    body = (
+      <Flex>
+        <Box mr={4}>{data.me.username}</Box>
+        <Button onClick={() => {
+          logout()
+        }} variant='link' isLoading={logoutFetching}>Logout</Button>
+      </Flex>
+    );
+  }
+
+  return (
+    <Flex bg="tan" p={3} ml={"auto"}>
+      <Box ml={"auto"}>{body}</Box>
+    </Flex>
+  );
+};
---
+++ src/pages/login.tsx
@@ -0,0 +1,63 @@
+import {
+  Box,
+  Button
+} from "@chakra-ui/core";
+import { Form, Formik } from "formik";
+import { withUrqlClient } from "next-urql";
+import { useRouter } from 'next/router';
+import React from "react";
+import { InputField } from "../components/InputField";
+import { Wrapper } from "../components/Wrapper";
+import { useLoginMutation } from "../generated/graphql";
+import { createUrqlClient } from "../utils/createUrqlClient";
+import { toErrorMap } from "../utils/toErrorMap";
+
+
+const Login: React.FC<{}> = ({}) => {
+  const router = useRouter();
+  const [, login] = useLoginMutation();
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ usernameOrEmail: "", password: "" }}
+        onSubmit={ async (values, {setErrors}) => {
+          const response = await login(values);
+          if(response.data?.login.errors){
+            setErrors(toErrorMap(response.data.login.errors))
+          } else if(response.data?.login.user){
+            //worked
+            router.push('/')
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="usernameOrEmail"
+              placeholder="username or email"
+              label="Username or Email"
+            />
+            <Box mt={6}>
+              <InputField
+                name="password"
+                placeholder="password"
+                label="Password"
+                type="password"
+              />
+            </Box>
+            <Button
+              mt={4}
+              type="submit"
+              variantColor="teal"
+              isLoading={isSubmitting}
+            >
+              login
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default withUrqlClient(createUrqlClient) (Login);
